99b8339237eff0845626323fbc8256df5cdde960
kevin <brissetkevin9@gmail.com>
2019-02-08 13:39:50 +0100

    commit 3

---
+++ src/main/java/s4/spring/td2/repositories/OrgaRepositoriy.java
@@ -0,0 +1,9 @@
+package s4.spring.td2.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import s4.spring.td2.entities.Organisation;
+
+public interface OrgaRepositoriy extends JpaRepository<Organisation, Integer>{
+
+}
