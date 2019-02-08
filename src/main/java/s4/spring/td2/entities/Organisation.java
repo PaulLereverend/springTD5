03c54cf85e369b408e0eeacd66f34efdc2d7a861
kevin <brissetkevin9@gmail.com>
2019-02-08 13:53:58 +0100

    commit 4

--- src/main/java/s4/spring/td2/entities/Organisation.java
+++ src/main/java/s4/spring/td2/entities/Organisation.java
@@ -12,4 +12,31 @@ public class Organisation {
 	private String nom;
 	private String domain;
 	private String aliases;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getDomain() {
+		return domain;
+	}
+	public void setDomain(String domain) {
+		this.domain = domain;
+	}
+	public String getAliases() {
+		return aliases;
+	}
+	public void setAliases(String aliases) {
+		this.aliases = aliases;
+	}
+	
+	
 }
--- src/main/java/s4/spring/td2/repositories/OrgaRepositoriy.java
+++ src/main/java/s4/spring/td2/repositories/OrgaRepositoriy.java
@@ -1,9 +1,11 @@
 package s4.spring.td2.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import s4.spring.td2.entities.Organisation;
 
+@Repository
 public interface OrgaRepositoriy extends JpaRepository<Organisation, Integer>{
 
 }
