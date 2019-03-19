3c282318b46468ceb6efef3a166ffd6efc612a63
PaulLereverend <nte.remix@gmail.com>
2019-03-19 13:42:18 +0100

    initial commit

--- src/main/java/s4/spring/td5/TD5Application.java
+++ src/main/java/s4/spring/td5/TD5Application.java
@@ -1,13 +1,13 @@
-package s4.spring.td2;
+package s4.spring.td5;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Td2Application {
+public class TD5Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Td2Application.class, args);
+		SpringApplication.run(TD5Application.class, args);
 	}
 
 }
